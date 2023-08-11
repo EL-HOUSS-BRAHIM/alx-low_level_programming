6fc6c15bd9a6e5858af5fcfc6be4d8d919af9d18
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:58:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return 0;
+}
