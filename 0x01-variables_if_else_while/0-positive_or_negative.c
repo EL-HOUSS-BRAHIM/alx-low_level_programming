5a066fc44edd237214db7f2012b1603499732b87
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:02:57 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("The value of n is: %d and it is positive\n", n);
+printf ("%d is positive\n", n);
 }
-if(n == 0)
+if (n == 0)
 {
-printf("The value of n is: %d and it is zero\n", n);
+printf ("%d is zero\n", n);
 }
-else{
-printf("The value of n is: %d and it is negative\n", n);
+else {
+printf ("%d is negative\n", n);
 }   
 return (0);
 }
