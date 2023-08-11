fc714a468a3fba9518786684c49f7fadeb9c52a6
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 16:43:40 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+char lowercase;
+for (n = 0; n >= 9; n++)
+{
+putchar(n);
+}
+for (lowercase = 'a'; n >= 'f'; lowercase++)
+{
+putchar(lowercase)
+}
+putchar('\n');
+return (0);
+}
