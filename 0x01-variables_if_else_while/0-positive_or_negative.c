3b358fb2e2a3d8e6474ddb8f715656da3d481cd5
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:10:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf ("%d is positive\n", n);
+printf("%d is positive\n", n);
 return (0)
 }
 if (n == 0)
 {
-printf ("%d is zero\n", n);
+printf("%d is zero\n", n);
 return (0)
 }
 else {
-printf ("%d is negative\n", n);
+printf("%d is negative\n", n);
 }   
 return (0);
 }
