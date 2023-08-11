1d0d1cf030b2ecdf05c7c28eb7639a3e66feec16
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 10:28:02 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; lowercase++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
