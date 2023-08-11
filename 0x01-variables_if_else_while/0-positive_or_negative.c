bcdf5668c74db45c9e93c1d7049175c00dc67533
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:20:05 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,19 @@
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-return (0)
-}
-if (n == 0)
-{
-printf("%d is zero\n", n);
-return (0)
-}
-else {
-printf("%d is negative\n", n);
-}   
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+    if (n > 0) {
+        printf("%d is positive\n", n);
+    }
+    if (n < 0) {
+    	printf("%d is negative\n", n);
+    }
+    if (n == 0) {
+        printf("%d is zero")
+    }
+
+	return (0);
 }
