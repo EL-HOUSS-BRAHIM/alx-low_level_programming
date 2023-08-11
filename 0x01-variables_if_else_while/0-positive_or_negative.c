42c0944d5d5bf9a2d348e26f9ad67a9e27fde48f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 08:41:23 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("The value of n is: %d\n", n);
+return (0);
+}
