8f79f36b8546973c968cab47219ee39af14b2878
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:53:42 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,15 +14,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("Last digit of %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("Last digit of %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 else if (lastDigit < 6)
 {
-printf("Last digit of %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
 return (0);
 }
