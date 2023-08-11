1892771c1e939938fa26c9d6d5f2b110b1851b5f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 16:35:31 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point of the program
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char n;
+for (n = 'z'; n <= 'a'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
