d92472f5bd2340a343a91b78b2bac6c457151ce5
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 09:08:37 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,12 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf ("%d is positive\n", n);
+return (0)
 }
 if (n == 0)
 {
 printf ("%d is zero\n", n);
+return (0)
 }
 else {
 printf ("%d is negative\n", n);
