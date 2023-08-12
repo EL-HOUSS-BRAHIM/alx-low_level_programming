916eba6a51656fefbb5894095f8bc8a66781c1d3
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-11 17:52:00 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,40 +5,24 @@
 */
 int main(void)
 {
-int num1, num2;
-for (num1 = 0; num1 <= 99; num1++)
+int num1 = 0, num2 = 1;
+while (num1 <= 98)
 {
-for (num2 = 0; num2 <= 99; num2++)
-{
-if (num1 <= num2)
-{
-if (num1 < 10)
-{
-putchar('0');
-putchar('0' + num1);
-}
-else
-{
-putchar('0' + num1 / 10);
-putchar('0' + num1 % 10);
-}
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-if (num2 < 10)
-{
-putchar('0');
-putchar('0' + num2);
-}
-else
-{
-putchar('0' + num2 / 10);
-putchar('0' + num2 % 10);
-}
-if (num1 != 99 || num2 != 98)
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
+if (num1 != 98)
 {
 putchar(',');
 putchar(' ');
 }
-}
+num2++;
+if (num2 > 99)
+{
+num1++;
+num2 = num1 + 1;
 }
 }
 putchar('\n');
