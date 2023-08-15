ba3d25c86331d6ff022bf2d95cf6d292033019f2
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-15 06:30:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
 #include "main.h"
-/**
-*  main -Entry point
-*  Return: Always 0 (Success)
-*/
 int main(void)
 {
-_putchar('_putchar\n');
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
 
 #endif /* MAIN_H */
