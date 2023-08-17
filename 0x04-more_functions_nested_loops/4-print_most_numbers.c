cee31f85cbcb0f003ff370f80b78e95f10c6f920
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 12:54:17 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ *Return: void
+ */
+
+void print_most_numbers(void)
+{
+  char c;
+  for (c = '0'; c <= '9'; c++) {
+    if (c != '2' && c != '4') {
+      _putchar(c);
+    }
+  }
+    _putchar('\n');
+}
