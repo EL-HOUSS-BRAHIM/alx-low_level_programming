3420b6f8a3547b53095610ed2c64b56d27492234
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 13:29:01 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
-* more_numbers - print more numbers
-*/
+ * more_numbers - Prints a sequence of numbers.
+ */
 void more_numbers(void)
 {
-int c;
-int j;
-for (c = 0; c < 10; c++)
-{
-for (j = 0; j <= 14; j++)
-{
-if(j >= 10) {
-_putchar('0' + j / 10);
-}
-_putchar('0' + j % 10);
-}
-_putchar('\n');
-}
+    int c;
+    int j;
+
+    for (c = 0; c < 10; c++)
+    {
+        for (j = 0; j <= 14; j++)
+        {
+            if (j >= 10)
+            {
+                _putchar('0' + j / 10);
+            }
+            _putchar('0' + j % 10);
+        }
+        _putchar('\n');
+    }
 }
