b42d44bd0bb07719da61603c233341a03312553d
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 16:14:01 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#pragma once
 #include <stdio.h>
-
 void positive_or_negative(int i);
-
-#endif /* MAIN_H */
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
