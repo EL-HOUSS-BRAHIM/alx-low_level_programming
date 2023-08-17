722249e4c2e4ba22e1c0fd1ebfd81f6253d2f66d
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 16:02:33 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -7,7 +6,7 @@
 int main(void)
 {
 int i;
-i = 0; // Change this line to test different values
+i = 0;
 positive_or_negative(i);
 return (0);
 }
