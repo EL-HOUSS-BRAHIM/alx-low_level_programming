748778f76d34f34a0b8f64da54af752eca03e012
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 12:55:49 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,13 @@
  *
  *Return: void
  */
-
 void print_most_numbers(void)
 {
-  char c;
-  for (c = '0'; c <= '9'; c++) {
-    if (c != '2' && c != '4') {
-      _putchar(c);
-    }
-  }
-    _putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++) {
+if (c != '2' && c != '4') {
+_putchar(c);
+}
+}
+_putchar('\n');
 }
