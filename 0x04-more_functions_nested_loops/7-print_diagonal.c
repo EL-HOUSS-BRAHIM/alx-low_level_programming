d7aa80a94fa5aeba22eccf7c706934f464bf8a65
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 15:31:26 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,9 +12,13 @@ void print_diagonal(int n) {
   }
   for (i = 0; i < n; i++) {
     for (j = 0; j < i; j++) {
-      _putchar(' ');
-    }
-    _putchar('\\');
-    _putchar('\n');
-  }
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
