0459b6a1cdde3903c23e009e1aa3d7e1bc455d9c
EL-HOUSS-BRAHIM <87819687+EL-HOUSS-BRAHIM@users.noreply.github.com>
2023-08-17 01:27:37 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,13 @@
 * Return: 0
 */ int main(void) 
 { _putchar('_'); 
- _putchar('p'); 
- _putchar('u'); 
- _putchar('t'); 
- _putchar('c'); 
- _putchar('h'); 
- _putchar('a'); 
- _putchar('r'); 
- _putchar('\n'); 
- return (0); 
+_putchar('p'); 
+_putchar('u'); 
+_putchar('t'); 
+_putchar('c'); 
+_putchar('h'); 
+_putchar('a'); 
+_putchar('r'); 
+_putchar('\n'); 
+return (0); 
 }
