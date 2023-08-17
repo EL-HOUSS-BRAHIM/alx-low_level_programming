d968e35e40b32a34a4db533c235572e532df07dd
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-17 15:23:28 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,21 +4,17 @@
  * print_diagonal - Draws a diagonal line of backslashes.
  * @n: The number of times to print the backslash character.
  */
-void print_diagonal(int n)
-{
+void print_diagonal(int n) {
   int i, j;
-  if (n <= 0)
-    {
-      _putchar('\n');
-        return;
+  if(n <= 0) {
+    _putchar('\n');
+    return;
+  }
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i; j++) {
+      _putchar(' ')
     }
-  for (i = 0; i < n; i++)
-    {
-      for (j = 0; j < i; j++)
-	{
-	  _putchar(' ');
-	}
-      _putchar('\\');
-      _putchar('\n');
-    }
+    _putchar('\\');
+    _putchar('\n');
+  }
 }
