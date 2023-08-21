593a9966850f1aa4eac4e4f11ed5221050f4ea3b
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-21 14:07:11 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - a function that prints a string, in reverse, followed by a new line.
+ * @s: a string.
+ * Return: Print string.
+ */
+void rev_string(char *s)
+{
+int i;
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (i = 0; i < length / 2; i++)
+    {
+        char temp = s[i];
+        s[i] = s[length - 1 - i];
+        s[length - 1 - i] = temp;
+    }
+}
