01faccdb02c1f2c65479e0d25dc9d39faeab43e8
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-21 14:10:07 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * rev_string - a function that prints a string, in reverse, followed by a new line.
+ * rev_string - a function that prints a string, in reverse.
  * @s: a string.
  * Return: Print string.
  */
@@ -15,9 +15,9 @@ while (s[length] != '\0')
 length++;
 }
 for (i = 0; i < length / 2; i++)
-    {
-        char temp = s[i];
-        s[i] = s[length - 1 - i];
-        s[length - 1 - i] = temp;
-    }
+{
+char temp = s[i];
+s[i] = s[length - 1 - i];
+s[length - 1 - i] = temp;
+}
 }
