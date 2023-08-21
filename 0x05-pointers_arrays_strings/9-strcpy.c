200fbe39576fa3bacee5359e69325df765fa1afb
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-21 14:56:48 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *original_dest = dest;
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
-    *dest = '\0';
-    return original_dest;
+char *original_dest = dest;
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return original_dest;
 }
