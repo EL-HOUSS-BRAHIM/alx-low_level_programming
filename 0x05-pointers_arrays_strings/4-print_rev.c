2d86ae84b1619d18c624380ff5d6d99b010a430a
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-21 13:50:03 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_rev - a function that prints a string, in reverse, followed by a new line.
+ * print_rev - function that prints a string, in reverse.
  * @s: a string.
  * @length: the length of the string.
  * @i: print the string in reverse
