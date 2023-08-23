6d45304df5f75ad2b0e61278ba91d629d6221902
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-23 16:09:06 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase letters to uppercase in a string.
+ * @str: The string to modify.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *string_toupper(char *str)
+{
+  char *ptr = str;
+  while (*ptr != '\0')
+    {
+      if (*ptr >= 'a' && *ptr <= 'z')
+	{
+	  *ptr = *ptr - 'a' + 'A';
+	}
+      ptr++;
+    }
+  return (str);
+}
