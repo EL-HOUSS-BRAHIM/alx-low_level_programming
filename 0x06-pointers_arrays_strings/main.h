fb8096b033b3b46f02d18b1dadca2b6df23c9262
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-23 16:37:24 -0700

    update

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #pragma once
 char *_strcat(char *dest, char *src);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
