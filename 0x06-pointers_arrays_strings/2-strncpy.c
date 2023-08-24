b168d3df6cbbeaf8d7b8b6919a982ee7c63a4306
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-23 17:03:45 -0700

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_strncpy - function that copies a string.
+ **_strncpy - function that copies a string.
  *@dest: the destination string.
  *@src: the source string.
  *@n: max char..
@@ -10,14 +10,14 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i;
-  for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  for (; i < n; i++)
-    {
-      dest[i] = '\0';
-    }
-  return (dest);
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+return (dest);
 }
