91637d79a64ab841a4e72745e6e643ad1b18923f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-23 17:21:09 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,10 +7,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-{
-s1++;
-s2++;
-}
-return (int)(*s1) - (int)(*s2);
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
