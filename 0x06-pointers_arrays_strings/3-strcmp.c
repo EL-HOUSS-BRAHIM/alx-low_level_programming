22f8265cd30394c8804a18b80d4facb08485f482
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-23 17:16:09 -0700

    update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,15 +4,16 @@
  * @s1: The first string.
  * @s2: The second string.
  *
- * Return: An integer less than, equal to, or greater than 0, depending on
+ * Return: An integer less than, equal to, or greater\
+ than 0, depending on
  * the comparison result (similar to strcmp).
  */
 int _strcmp(char *s1, char *s2)
 {
-  while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
-    {
-      s1++;
-      s2++;
-    }
-  return (int)(*s1) - (int)(*s2);
+while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
+{
+s1++;
+s2++;
+}
+return (int)(*s1) - (int)(*s2);
 }
