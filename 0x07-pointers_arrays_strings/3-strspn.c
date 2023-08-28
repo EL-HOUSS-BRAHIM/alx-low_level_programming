d7ebb84a5c4a47ae675590128088040b4e5a9251
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-08-28 16:48:57 -0700

    3-strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: Pointer to the string to be searched.
+ * @accept: Pointer to the string containing characters to match.
+ *
+ * Return: Number of bytes in the initial segment of @s
+ *         which consist only of bytes from @accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0;
+while (*s != '\0')
+{
+  char *a = accept;
+while (*a != '\0')
+{
+if (*s == *a)
+{
+count++;
+break;
+}
+a++;
+}
+if (*a == '\0')
+break;
+s++;
+}
+return (count);
+}
