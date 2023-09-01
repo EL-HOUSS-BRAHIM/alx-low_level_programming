a029305749bf4752f936cfcc479b5917b4ec33b8
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-01 12:56:02 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a quote by Kevin Mitnick using _puts.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 _puts("\"At the end of the day, my goal was to be the best hacker\"\n"
