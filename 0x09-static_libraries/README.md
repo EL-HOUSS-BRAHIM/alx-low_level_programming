2643342fe97b50ef578834e7299d47cce3bec932
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-01 14:12:22 -0700

    0x09-static_libraries

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,1 @@
-0x09-static_libraries
+Static Libaries
--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,3 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
