37435d7e011bc925e33482491351152933dde23d
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-05 15:08:17 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,17 +9,17 @@
  */
 char *_strdup(char *str)
 {
-  char *duplicate;
-  unsigned int length = 0;
-  unsigned int i;
-  if (str == NULL)
-    return (NULL);
-  while (str[length])
-    length++;
-  duplicate = malloc((length + 1) * sizeof(char));
-  if (duplicate == NULL)
-    return (NULL);
-  for (i = 0; i < length; i++)
-    duplicate[i] = str[i];
-  return (duplicate);
+char *duplicate;
+unsigned int length = 0;
+unsigned int i;
+if (str == NULL)
+return (NULL);
+while (str[length])
+length++;
+duplicate = malloc((length + 1) * sizeof(char));
+if (duplicate == NULL)
+return (NULL);
+for (i = 0; i < length; i++)
+duplicate[i] = str[i];
+return (duplicate);
 }
