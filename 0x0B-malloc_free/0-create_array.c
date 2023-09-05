cc1bbe50d40d5caed9e8deb5a5d8fd1f263253d7
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-05 14:50:25 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
 char *arr;
 unsigned int i;
 if (size == 0)
-return NULL;
+return (NULL);
 arr = malloc(size * sizeof(char));
 if (arr == NULL)
-return NULL;
+return (NULL);
 for (i = 0; i < size; i++)
 {
 arr[i] = c;
