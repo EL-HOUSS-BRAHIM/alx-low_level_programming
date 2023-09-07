523b31b4c93e32fe74a3675d5da73742484f5407
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-07 16:57:13 -0700

    advanced

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,38 +1,72 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
-**/
-
-int main(int argc, char *argv[])
-{
-	unsigned long mul;
-	int i, j;
-
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{
-				printf("Error\n");
-				exit(98);
-			}
-		}
-	}
-	mul = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", mul);
-	return (0);
+ * is_digit - Check if a character is a digit
+ * @c: The character to check
+ * Return: 1 if the character is a digit, 0 otherwise
+ */
+int is_digit(char c) {
+return (c >= '0' && c <= '9');
+}
+/**
+ * main - Multiply two positive numbers
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ * Return: 0 on success, 98 on error
+ */
+int main(int argc, char *argv[]) {
+if (argc != 3) {
+printf("Error\n");
+return (98);
+}
+char *num1 = argv[1];
+char *num2 = argv[2];
+for (int i = 0; num1[i] != '\0'; i++) {
+if (!is_digit(num1[i])) {
+printf("Error\n");
+return (98);
+}
+}
+for (int i = 0; num2[i] != '\0'; i++) {
+if (!is_digit(num2[i])) {
+printf("Error\n");
+return (98);
+}
+}
+int len1 = 0;
+int len2 = 0;
+int *result;
+int carry = 0;
+while (num1[len1])
+len1++;
+while (num2[len2])
+len2++;
+result = malloc(sizeof(int) * (len1 + len2));
+if (!result) {
+printf("Error\n");
+return (98);
+}
+for (int i = 0; i < len1 + len2; i++)
+result[i] = 0;
+for (int i = len1 - 1; i >= 0; i--) {
+carry = 0;
+for (int j = len2 - 1; j >= 0; j--) {
+int mul = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+result[i + j + 1] = mul % 10;
+carry = mul / 10;
+}
+result[i] += carry;
+}
+int started = 0;
+for (int i = 0; i < len1 + len2; i++) {
+if (result[i] != 0)
+started = 1;
+if (started)
+printf("%d", result[i]);
+}
+if (!started)
+printf("0");
+printf("\n");
+free(result);
+return (0);
 }
