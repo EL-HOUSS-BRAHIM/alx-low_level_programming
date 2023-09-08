25dd8477b2b99fdfa0a336f8fbb69e152effb1b3
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-07 17:19:04 -0700

    advanced

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,10 @@
 #pragma once
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
+#include <stdio.h>
+#include <stdlib.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
