3ad487fb1090b4e1e83db7bc3484c6e4c4f82ede
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-11 12:09:25 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,8 +9,8 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name: %s\n", d->name !=NULL ? d->name : "(null)");
+printf("Name: %s\n", d->name !=NULL ? d->name : "(nil)");
 printf("Age: %.6f\n", d->age);
-printf("Owner: %s\n", d->owner != NULL ? d->owner : "(null)");
+printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
 }
 }
