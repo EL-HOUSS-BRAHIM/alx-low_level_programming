10c2f226d7045c4cf401240e60f71d7b1ee99e2e
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-11 12:12:46 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+struct dog my_dog;
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dog.owner = "Bob";
+print_dog(&my_dog);
+return (0);
 }
