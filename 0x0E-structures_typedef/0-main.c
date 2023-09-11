4b77783f83c66a6afbac8dca7de1e73adc058e4b
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-11 11:47:02 -0700

    0-main.c

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -8,11 +8,10 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+struct dog my_dog;
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dog.owner = "Bob";
+printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+return (0);
 }
