d2e23813a4ccea356f85cf08dd909f79a346536f
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-13 13:35:30 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,11 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 /**
  * print_name - prints a name based on the provided function
  * @name: name to be printed
  * @f: function pointer to specify the printing format
+ *
+ * Return: None
  */
 void print_name(char *name, void (*f)(char *))
 {
