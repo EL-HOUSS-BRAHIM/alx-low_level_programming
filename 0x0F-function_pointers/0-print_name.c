7d1cca6602a1ad98efdc87e481ad5b1d13de7631
EL-HOUSS-BRAHIM <R130327753@taalim.ma>
2023-09-13 13:26:34 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * print_name - prints a name based on the provided function
+ * @name: name to be printed
+ * @f: function pointer to specify the printing format
+ *
+ * Return: None
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL && f != NULL)
+f(name);
+}
